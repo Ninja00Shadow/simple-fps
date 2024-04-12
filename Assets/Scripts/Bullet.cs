165f177115bb0d25192301ba8fc7849491028447
Dawid <mrshadow813@gmail.com>
2024-04-12 22:40:39 +0200

    bullet hole effect

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -10,13 +10,32 @@ public class Bullet : MonoBehaviour
         if (other.gameObject.CompareTag("Target"))
         {
             print("hit " + other.gameObject.name);
+            
+            CreateBulletImpactEffect(other);
+            
             Destroy(gameObject);
         }
 
         if (other.gameObject.CompareTag("Wall"))
         {
             print("hit wall");
+            
+            CreateBulletImpactEffect(other);
+            
             Destroy(gameObject);
         }
+    }
+
+    void CreateBulletImpactEffect(Collision objectWeHit)
+    {
+        ContactPoint contact = objectWeHit.contacts[0];
+
+        GameObject hole = Instantiate(
+            GlobalReferences.Instance.bulletImpactEffectPrefab,
+            contact.point,
+            Quaternion.LookRotation(contact.normal)
+            );
+        
+        hole.transform.SetParent(objectWeHit.gameObject.transform);
     }
 }
